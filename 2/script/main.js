26e4ddac7b6f74656ef8010760b13fff0c61a6e2
AzzCoding <aziziegatrimuthi16@gmail.com>
2024-08-29 17:01:57 +0700

    tes

--- 2/script/main.js
+++ 2/script/main.js
@@ -53,39 +53,66 @@ const animationTimeline = () => {
     tl.to(".container", 0.6, {
         visibility: "visible"
     })
-    .from(".one", 0.7, {
+    .from(".zero", 0.7, {
         opacity: 0,
         y: 10
     })
-    .from(".two", 0.4, {
+    .from(".one", 0.4, {
         opacity: 0,
         y: 10
     })
-    .to(".one",
+    .to(".zero",
         0.7,
         {
             opacity: 0,
             y: 10
         },
     "+=3.5")
-    .to(".two",
+    .to(".one",
         0.7,
         {
             opacity: 0,
             y: 10
         },
     "-=1")
-    .from(".three", 0.7, {
+    .from(".two", 0.7, {
         opacity: 0,
         y: 10
     })
-    .to(".three",
+    .to(".two",
         0.7,
         {
             opacity: 0,
             y: 10
         },
     "+=3")
+    .from(".three", 0.7, {
+        scale: 0.2,
+        opacity: 0,
+    })
+    .from(".fake-btn", 0.3, {
+        scale: 0.2,
+        opacity: 0,
+    })
+    .staggerTo(
+        ".hbd-chatbox span",
+        1.5, {
+            visibility: "visible",
+        },
+        0.05
+    )
+    .to(".fake-btn", 0.1, {
+        backgroundColor: "rgb(127, 206, 248)",
+    },
+    "+=4")
+    .to(
+        ".three",
+        0.5, {
+            scale: 0.2,
+            opacity: 0,
+            y: -150
+        },
+    "+=1")
     .from(".four", 0.7, {
         scale: 0.2,
         opacity: 0,
